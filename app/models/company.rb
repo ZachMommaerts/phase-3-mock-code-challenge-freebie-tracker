49881a19fd5aa9a89653e66e00b3192a405e69c0
Zach <zmommaerts@gmail.com>
2021-12-22 20:50:12 -0600

    commit tree e6540ec1a2751ca0bcee7c9dbd3cc09a75873234

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,13 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
 
+    def give_freebie(dev, item_name, value)
+        selected_dev = Dev.where(':name = ?', dev)
+        Freebie.create(company_id: self.id, dev_id: selected_dev.id, item_name: 'maracas', value: 3)
+    end
+
+    def self.oldest_company
+        self.sort{ |a,b| a.founding_year <=> b.founding_year}.first
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,12 @@ Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
+
+Freebie.create(dev_id: 1, company_id: 1, item_name: 'frisbee', value: 1)
+Freebie.create(dev_id: 2, company_id: 2, item_name: 'keychain', value: 5)
+Freebie.create(dev_id: 3, company_id: 3, item_name: 'mug', value: 6)
+Freebie.create(dev_id: 4, company_id: 4, item_name: 'pencil', value: 0)
+Freebie.create(dev_id: 5, company_id: 5, item_name: 'speaker', value: 10)
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
